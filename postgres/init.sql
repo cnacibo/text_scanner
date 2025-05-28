b41fa0e58cd12627c63a679e1a7bea97e2f67ad1
Vlasova Mariia <vlasovam014@gmail.com>
2025-05-28 05:07:31 +0300

    clean storage

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,14 +1,14 @@
 -- postgres/init.sql
 
--- Создаем analysis DB и пользователя
+-- создаем analysis DB и пользователя
 CREATE USER analysis_user WITH PASSWORD 'analysis_pass';
 CREATE DATABASE analysis;
 GRANT ALL PRIVILEGES ON DATABASE analysis TO analysis_user;
 
--- Подключаемся к БД files (создана через POSTGRES_DB в docker-compose)
+-- подключаемся к БД files
 \connect files;
 
--- Создаем таблицу files
+-- создаем таблицу files
 CREATE TABLE IF NOT EXISTS files (
     id UUID PRIMARY KEY,
     filename TEXT NOT NULL,
